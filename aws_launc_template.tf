9f731f9ccd8bd8d6745a5a36874961e7b39e7733
Just <Justvanthiel@gmail.com>
2025-10-02 20:31:47 +0200

    edited node export version

--- aws_launc_template.tf
+++ aws_launc_template.tf
@@ -57,9 +57,10 @@ sudo systemctl restart apache2
 # -------------------------
 NODE_EXPORTER_VERSION="1.8.1"
 cd /tmp
-wget https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
-tar xvf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
-sudo mv node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter /usr/local/bin/
+wget https://github.com/prometheus/node_exporter/releases/download/v\${NODE_EXPORTER_VERSION}/node_exporter-\${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+tar xvf node_exporter-\${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+sudo mv node_exporter-\${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter /usr/local/bin/
+
 
 # Create node_exporter user
 sudo useradd -rs /bin/false node_exporter
